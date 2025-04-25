64ee52f5eb0a5e2950b5d1f942933aba0b8c1069
ForexOL <zantamore@gmail.com>
2025-04-25 13:47:01 +0300

    update

--- myshop/settings.py
+++ myshop/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS = (
 STATIC_ROOT=os.path.join(BASE_DIR ,'staticfiles')
 
 # Optional: compress static files
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
