40b98b0e09aa066277ac7aadc72d9b5f30be2bba
ForexOL <zantamore@gmail.com>
2025-05-06 01:30:06 +0300

    Update settings.py

--- myshop/settings.py
+++ myshop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+(ocq$n8k7q0c^%gz5w%lxcu8wawq2#^%gznr7vt*0&-)a5z4o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['must.gigz.store','137.184.78.92','*']
+ALLOWED_HOSTS = ['must.gigz.store','www.must.gigz.store','137.184.78.92','*']
 
 
 # Application definition
